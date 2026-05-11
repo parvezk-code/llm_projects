040338e81f9f5006b7c285a21265d43d44c7202b
parvezk <parvezk>
2026-05-11 23:39:39 +0530

    mcp pdf chat

--- pdf-chat-system-03-mcp/services/service_composer.py
+++ pdf-chat-system-03-mcp/services/service_composer.py
@@ -1,14 +1,18 @@
 # services/service_composer.py
 
-from services.pdf.pdf_service import PDFService
-from services.pdf.pdf_controller import PDFController
+from services.document_extractors.pdf.pymupdf.controller import PyMuPDFController
+from services.document_extractors.pdf.pymupdf.service import PyMuPDFService
+
 from services.llm.llm_service import LLMService
 from services.llm.llm_controller import LLMController
+from services.llm.utils.openai_formatter import OpenAIFormatter
+
+from services.mcp.clients.client_sync import SyncConnection
+from services.mcp.executor.controller import MCPToolController
+
 from services.service_bundle import ServiceBundle
 from conf.settings.config_bundle import AppSettings
-from services.mcp.clients.client_sync import SyncConnection
-from services.mcp.controller import MCPToolController
-from services.llm.utils.openai_formatter import OpenAIFormatter
+
 
 class ServiceComposer:
 
@@ -17,16 +21,16 @@ class ServiceComposer:
         # --- Load settings ---
         settings = AppSettings()
 
-        # --- Build PDF service and controller ---
-        pdf_service    = PDFService()
-        pdf_controller = PDFController(pdf_service)
+        # --- Build PDF extractor ---
+        pdf_service    = PyMuPDFService()
+        pdf_controller = PyMuPDFController(pdf_service)
 
-        # --- Build MCP Client service and controller ---
+        # --- Build MCP client, service, registry and controller ---
         mcp_client     = SyncConnection(settings.mcpConfig.mcp_server_url)
         mcp_controller = MCPToolController(mcp_client)
-        available_tools= OpenAIFormatter().format_tool_definitions(mcp_controller.get_tools_list())
-        
+
         # --- Build LLM service and controller ---
+        available_tools = OpenAIFormatter().format_tool_definitions(mcp_controller.get_tools_list())
         llm_service    = LLMService(
             api_key     = settings.llm.api_key,
             model       = settings.llm.model,
@@ -37,7 +41,7 @@ class ServiceComposer:
 
         # --- Return frozen bundle ---
         return ServiceBundle(
-            pdf=pdf_controller,
-            llm=llm_controller,
-            mcp=mcp_controller
+            pdf = pdf_controller,
+            llm = llm_controller,
+            mcp = mcp_controller
         )
